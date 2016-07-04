05cb3f5872779957d330112124c54f228a3a122e
Priti <pripra3014@gmail.com>
2016-07-04 14:17:03 +0530

    add register and login files

--- javascripts/chirpApp.js
+++ javascripts/chirpApp.js
@@ -11,3 +11,22 @@ app.controller('mainController', function($scope){
     };
 
 });
+
+app.controller('authController', function($scope){
+  $scope.user = {username: '', password: ''};
+  $scope.error_message = '';
+
+  postService.getAll().success(function(data){
+    $scope.posts = data;
+  });
+
+  $scope.login = function(){
+    //placeholder until authentication is implemented
+    $scope.error_message = 'login request for ' + $scope.user.username;
+  };
+
+  $scope.register = function(){
+    //placeholder until authentication is implemented
+    $scope.error_message = 'registeration request for ' + $scope.user.username;
+  };
+});
